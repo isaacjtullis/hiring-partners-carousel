68f52087832ad032aa86a41f5a6c9589965fcf21
Isaac Tullis <isaacjtullis@gmail.com>
2017-02-19 08:16:56 -0700

    Added rotating photos to my application

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import ImageList from './ImageList.js';
 
-const App = props => {
-  return(
-    <div>
-      <ImageList images={props.data}/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      photoIndex: 0,
+      maxIndex: this.props.data.length - 1
+    }
+    this.updateIndex = this.updateIndex.bind(this)
+  }
+
+  updateIndex(){
+    if (this.state.photoIndex === this.state.maxIndex){
+      this.setState({photoIndex: 0})
+    } else {
+      let newIndex = this.state.photoIndex + 1
+      this.setState({photoIndex: newIndex })
+    }
+  }
+
+  componentWillMount(){
+    setInterval(this.updateIndex, 2000);
+  }
+
+  render() {
+    let x = this.state.photoIndex
+
+    return (
+      <div>
+        <img src={this.props.data[x]}/>
+      </div>
+    );
+  };
 };
 export default App;
